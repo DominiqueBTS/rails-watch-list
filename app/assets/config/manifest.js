2bf77238d3606db2d2f42e15aebdcf523b21130d
Dominique <dominiquebertisch@gmail.com>
2025-02-16 21:33:36 +0100

    created tables for movies and lists

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,7 @@
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+
+// app/assets/config/manifest.js
+//= link popper.js
+//= link bootstrap.min.js

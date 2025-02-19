c6f04c8f84011db2d7253dae24836b18f85456b4
Dominique <dominiquebertisch@gmail.com>
2025-02-19 16:58:55 +0100

    halfway in the lists

--- app/controllers/list_controller.rb
+++
@@ -1,7 +0,0 @@
-class ListController < ApplicationController
-
-  def new
-    @movie = Movie.find(params[:movie_id])
-    @list = List.new
-  end
-end
--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,4 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   # has_many :movies
   validates :name, presence: true, uniqueness: true
-  validates :list, presence: true
 end
